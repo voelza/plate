4aa0669bf675e173ebf8b9ef43544b04b125515f
Achim Völz <achim.voelz.1991@gmail.com>
2022-08-06 15:36:43 +0200

    made UnsafeHTMLRender stream properly

--- src/main/java/com/voelza/plate/view/UnsafeHTMLRender.java
+++ src/main/java/com/voelza/plate/view/UnsafeHTMLRender.java
@@ -4,6 +4,7 @@ import com.voelza.plate.html.Attribute;
 import com.voelza.plate.html.Element;
 import com.voelza.plate.html.HTMLParser;
 
+import java.io.PrintWriter;
 import java.util.List;
 
 class UnsafeHTMLRender implements ElementRender {
@@ -20,9 +21,19 @@ class UnsafeHTMLRender implements ElementRender {
 
     @Override
     public ElementRenderResult renderHTML(final RenderContext renderContext) {
+        final List<ElementRender> renders = getElementRenders(renderContext);
+        return Renderer.render(renders, renderContext);
+    }
+
+    @Override
+    public ElementStreamResult stream(final PrintWriter printWriter, final RenderContext renderContext) {
+        final List<ElementRender> renders = getElementRenders(renderContext);
+        return Renderer.stream(printWriter, renders, renderContext);
+    }
+
+    private List<ElementRender> getElementRenders(final RenderContext renderContext) {
         final String html = String.valueOf(renderContext.expressionResolver().evaluate(htmlExpression));
         final List<Element> elements = HTMLParser.parse(html).getElements();
-        final List<ElementRender> renders = RenderCreator.create(options.newElements(elements));
-        return Renderer.render(renders, renderContext);
+        return RenderCreator.create(options.newElements(elements));
     }
 }
