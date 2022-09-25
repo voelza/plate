39c57a7b4dc68c35e6674f98824f0646c528a421
Achim Völz <achim.voelz.1991@gmail.com>
2022-09-25 18:09:07 +0200

    added new test

---
+++ src/test/resources/TestWithMainPath/result/index.js
@@ -0,0 +1,13 @@
+const plate = function() {
+    function setup(selector, setupFunc) {
+        document
+        .querySelectorAll(selector)
+        .forEach(e => {
+            const uuid=e.previousSibling?.textContent ?? 'main';
+            setupFunc({element:e,props:plateModel[uuid]});
+        });
+    }
+    setup('[data-p-setup-test]',({element,props}) => {
+      console.log("hello");
+    });
+}();
